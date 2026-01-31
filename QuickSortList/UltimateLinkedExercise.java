ab04d92478c75c81c6f4ba81d90c09c3f1528590
JoshuaJeong7 <jeong.joshua275@gmail.com>
2026-01-30 17:07:11 -0800

    Initially created UltimateLinkedExercise skeleton code and methods

---
+++ QuickSortList/UltimateLinkedExercise.java
@@ -0,0 +1,24 @@
+
+class UltimateLinkedExercise {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        
+    }
+    
+    public static void sortList(UltimateList root) {
+        
+    }
+    
+    public static boolean isSorted(UltimateList root) {
+        
+    }
+}
+
+class UltimateList {
+    int val;
+    UltimateList next;
+    public UltimateList(int valee) {
+        val = valee;
+        next = null;
+    }
+}
